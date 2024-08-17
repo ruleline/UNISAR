49a21bd1e24bd109d5a2662458e3e2f745ce0436
ruleline <ruleline@outlook.com>
2024-08-18 02:46:46 +0800

    init🎉: 初版

---
+++ README.md
@@ -0,0 +1,12 @@
+# ARCH
+
+打造一款适用于嵌入式汽车电子 MCU 方面的通用架构。
+
+* C 语言
+* GCC 编译构建
+* 面向对象思想
+* 使用 OS
+
+## 01. 架构
+
+![architecture](./documentation/architecture/architecture.png)
---
+++ source/required/application/main.c
@@ -0,0 +1,30 @@
+
+/**
+ * @file main.c
+ * @brief main
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2024-04-02
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2024-08-18
+ * @version 0.00.001
+ *
+ * @copyright ©2024 ruleline
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |       by       |              comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2024-04-02 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * @brief 主任务
+ *
+ * @retval 0
+ */
+int
+main(void) {
+        return (0);
+} /* main */
