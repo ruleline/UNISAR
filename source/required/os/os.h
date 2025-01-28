3af4eb99e1fe451b11604521be7c6cc35b98544e
ruleline <ruleline@outlook.com>
2025-01-28 12:45:25 +0800

    file📦: os.h

---
+++ source/required/os/os.h
@@ -0,0 +1,33 @@
+/**
+ * @file os.h
+ * @brief os
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-01-28
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-01-28
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-01-28 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined OS_H
+#define OS_H
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "timers.h"
+#include "event_groups.h"
+#include "stream_buffer.h"
+#include "message_buffer.h"
+
+#endif /* !defined OS_H */
