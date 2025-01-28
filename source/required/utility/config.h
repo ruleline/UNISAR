269fc2122655b6826a846e41b95e4a6f45b2b4f1
ruleline <ruleline@outlook.com>
2025-01-28 12:44:58 +0800

    file📦: config.h

---
+++ source/required/utility/config.h
@@ -0,0 +1,26 @@
+/**
+ * @file config.h
+ * @brief config
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-01-28
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-01-28
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-01-28 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined CONFIG_H
+#define CONFIG_H
+
+
+
+#endif /* !defined CONFIG_H */
