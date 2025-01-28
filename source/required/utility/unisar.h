4cbddfab91f77023e52dec379c97b358e4a41a55
ruleline <ruleline@outlook.com>
2025-01-28 12:45:45 +0800

    file📦: unisar.h

---
+++ source/required/utility/unisar.h
@@ -0,0 +1,28 @@
+/**
+ * @file unisar.h
+ * @brief unisar
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-01-28
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-01-28
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-01-28 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined UNISAR_H
+#define UNISAR_H
+
+#include "config.h"
+#include "standard.h"
+#include "os.h"
+
+#endif /* !defined UNISAR_H */
