161bc6eac89244758a8f2cd6acc6de94587c7f2a
ruleline <ruleline@outlook.com>
2025-01-28 12:19:01 +0800

    file📦: 增加standard.h

---
+++ source/required/utility/standard.h
@@ -0,0 +1,36 @@
+/**
+ * @file standard.h
+ * @brief standard
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-01-28
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-01-28
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-01-28 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined STANDARD_H
+#define STANDARD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <time.h>
+
+#endif /* !defined STANDARD_H */
