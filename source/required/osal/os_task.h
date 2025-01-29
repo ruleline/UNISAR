3d28a4ff8c7e6930e1927532cd9034a2556f8b7e
ruleline <ruleline@outlook.com>
2025-01-29 14:26:02 +0800

    feat✨: 增加os挂起所有任务

--- source/required/osal/os_task.h
+++ source/required/osal/os_task.h
@@ -104,6 +104,21 @@ static inline long task_suspend_(struct TASK *task)
 }
 
 /**
+ * @brief 挂起所有任务
+ *
+ * @param[in] task 任务
+ * @return 结果
+ * @retval 0 成功
+ */
+static inline long task_suspend_all_(void)
+{
+        #if ((FREERTOS == 1) && (FREERTOS_TASK == 1))
+        vTaskSuspendAll();
+        #endif /* ((FREERTOS == 1) && (FREERTOS_TASK == 1)) */
+        return (0);
+}
+
+/**
  * @brief 恢复任务
  *
  * @param[in] task 任务
