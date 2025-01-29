a9d8e429a4383340cc6e9f07868b6612dbf289dc
ruleline <ruleline@outlook.com>
2025-01-29 14:17:52 +0800

    feat✨: 增加os绝对延时

--- source/required/osal/os_task.h
+++ source/required/osal/os_task.h
@@ -74,18 +74,42 @@ static inline long task_delete_(struct TASK *task)
 }
 
 /**
- * @brief 延时
+ * @brief 相对延时
  *
  * @param[in] time 时间(单位:ms)
  * @return 结果
  * @retval 0 成功
  */
-static inline long task_delay_(unsigned long time)
+static inline long general_delay_(unsigned long time)
 {
         #if ((FREERTOS == 1) && (FREERTOS_TASK == 1))
         vTaskDelay(pdMS_TO_TICKS(time));
         #endif /* ((FREERTOS == 1) && (FREERTOS_TASK == 1)) */
         return (0);
+}
+
+/**
+ * @brief 绝对延时
+ *
+ * @param[in,out] start 开始时间(单位:ms)
+ * @param[in] time 时间(单位:ms)
+ * @return 结果
+ * @retval 0 成功
+ * @retval -1 失败
+ */
+static inline long precise_delay_(unsigned long *start, unsigned long time)
+{
+        _Bool is_delay = 0;
+
+        #if ((FREERTOS == 1) && (FREERTOS_TASK == 1))
+        is_delay = xTaskDelayUntil(pdMS_TO_TICKS(start), pdMS_TO_TICKS(time));
+        #endif /* ((FREERTOS == 1) && (FREERTOS_TASK == 1)) */
+
+        if (is_delay) {
+                return 0;
+        } else {
+                return -1;
+        }
 }
 
 #endif /* !defined OS_TASK_H */
