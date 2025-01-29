7603de2d69ce9a872ee72433f2b33978f07ec494
ruleline <ruleline@outlook.com>
2025-01-29 21:30:50 +0800

    feat✨: 增加os解递归锁

--- source/required/osal/os_semaphore.h
+++ source/required/osal/os_semaphore.h
@@ -176,4 +176,28 @@ static inline long semaphore_recursive_lock_(struct SEMAPHORE *semaphore)
         }
 }
 
+/**
+ * @brief 解递归锁
+ *
+ * @param[in] semaphore 信号量
+ * @return 结果
+ * @retval 0 成功
+ * @retval -1 失败
+ */
+static inline long semaphore_recursive_unlock_(struct SEMAPHORE *semaphore)
+{
+        _Bool is_unlock = 0;
+
+        #if ((FREERTOS == 1) && (FREERTOS_SEMAPHORE == 1))
+        is_unlock = xSemaphoreGiveRecursive(semaphore->handle);
+        is_unlock = (is_unlock == pdTRUE);
+        #endif /* ((FREERTOS == 1) && (FREERTOS_SEMAPHORE == 1)) */
+
+        if (is_unlock) {
+                return (0);
+        } else {
+                return (-1);
+        }
+}
+
 #endif /* !defined OS_SEMAPHORE_H */
