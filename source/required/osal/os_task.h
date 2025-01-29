367f8840ad5b4af876a8cdfc9f5ce1f3edab1d03
ruleline <ruleline@outlook.com>
2025-01-29 14:26:34 +0800

    feat✨: 增加os恢复所有任务

--- source/required/osal/os_task.h
+++ source/required/osal/os_task.h
@@ -134,6 +134,21 @@ static inline long task_resume_(struct TASK *task)
 }
 
 /**
+ * @brief 恢复所有任务
+ *
+ * @param[in] task 任务
+ * @return 结果
+ * @retval 0 成功
+ */
+static inline long task_resume_all_(void)
+{
+        #if ((FREERTOS == 1) && (FREERTOS_TASK == 1))
+        xTaskResumeAll();
+        #endif /* ((FREERTOS == 1) && (FREERTOS_TASK == 1)) */
+        return (0);
+}
+
+/**
  * @brief 绝对延时
  *
  * @param[in,out] start 开始时间(单位:ms)
