3b35b9ce491b2ec7ba0583f72596c4295d5e6a69
ruleline <ruleline@outlook.com>
2025-02-08 21:47:50 +0800

    perf👌: 修改配置

--- source/required/utility/config/config.h
+++ source/required/utility/config/config.h
@@ -30,15 +30,15 @@
 
 typedef _Bool                   bool;
 typedef signed char             i8;
-typedef signed short            i16;
+typedef signed short int        i16;
 typedef signed int              i32;
-typedef signed long long        i64;
-typedef signed long             isize;
+typedef signed long long int    i64;
+typedef signed long int         isize;
 typedef unsigned char           u8;
-typedef unsigned short          u16;
+typedef unsigned short int      u16;
 typedef unsigned int            u32;
-typedef unsigned long long      u64;
-typedef unsigned long           usize;
+typedef unsigned long long int  u64;
+typedef unsigned long int       usize;
 typedef float                   f32;
 typedef double                  f64;
 
