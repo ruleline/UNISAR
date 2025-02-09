9b5e1b95e1f39716d06c8b54eac8de7e42c850ff
ruleline <ruleline@outlook.com>
2025-02-09 20:58:10 +0800

    feat✨: unisis增加init

---
+++ source/required/unisis/init/init.h
@@ -0,0 +1,26 @@
+/**
+ * @file init.h
+ * @brief init
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-09
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-09
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-09 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined INIT_H
+#define INIT_H
+
+
+
+#endif /* !defined INIT_H */
---
+++ source/required/unisis/init/init_config.h
@@ -0,0 +1,26 @@
+/**
+ * @file init_config.h
+ * @brief init
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-09
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-09
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-09 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined INIT_CONFIG_H
+#define INIT_CONFIG_H
+
+
+
+#endif /* !defined INIT_CONFIG_H */
