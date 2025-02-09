1269dfd5495b57e88764c6713d36e76fa8c298b3
ruleline <ruleline@outlook.com>
2025-02-09 20:33:19 +0800

    perf👌: freertos-使能获取任务状态

--- source/optional/open/freertos/include/FreeRTOSConfig.h
+++ source/optional/open/freertos/include/FreeRTOSConfig.h
@@ -638,7 +638,7 @@
 #define INCLUDE_xTaskGetCurrentTaskHandle      1
 #define INCLUDE_uxTaskGetStackHighWaterMark    0
 #define INCLUDE_xTaskGetIdleTaskHandle         0
-#define INCLUDE_eTaskGetState                  0
+#define INCLUDE_eTaskGetState                  1
 #define INCLUDE_xEventGroupSetBitFromISR       1
 #define INCLUDE_xTimerPendFunctionCall         0
 #define INCLUDE_xTaskAbortDelay                0
