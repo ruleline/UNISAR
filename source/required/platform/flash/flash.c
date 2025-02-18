913530fb4d1e9fa88c273fd71bfeb9bda0891945
ruleline <ruleline@outlook.com>
2025-02-19 00:01:48 +0800

    file📦: [platform][flash]增加初始文件

---
+++ source/required/platform/flash/flash.c
@@ -0,0 +1,21 @@
+/**
+ * @file flash.c
+ * @brief flash
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-19
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-19
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-19 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "flash.h"
---
+++ source/required/platform/flash/flash.h
@@ -0,0 +1,26 @@
+/**
+ * @file flash.h
+ * @brief flash
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-19
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-19
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-19 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined FLASH_H
+#define FLASH_H
+
+
+
+#endif /* !defined FLASH_H */
