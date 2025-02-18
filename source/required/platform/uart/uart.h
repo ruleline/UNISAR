d848065d564b1a56ecb9a3692956aedce8d70562
ruleline <ruleline@outlook.com>
2025-02-18 23:52:02 +0800

    file📦: [platform][uart]增加初始文件

---
+++ source/required/platform/uart/uart.h
@@ -0,0 +1,26 @@
+/**
+ * @file uart.h
+ * @brief uart
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined UART_H
+#define UART_H
+
+
+
+#endif /* !defined UART_H */
