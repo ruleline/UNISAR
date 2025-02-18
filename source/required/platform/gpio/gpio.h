a8aff3b28115ef22f8f724a87a0842c24cc74e03
ruleline <ruleline@outlook.com>
2025-02-18 23:58:46 +0800

    file📦: [platform][gpio]增加初始文件

---
+++ source/required/platform/gpio/gpio.h
@@ -0,0 +1,26 @@
+/**
+ * @file gpio.h
+ * @brief gpio
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined GPIO_H
+#define GPIO_H
+
+
+
+#endif /* !defined GPIO_H */
