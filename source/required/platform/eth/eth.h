cd736dfba925df6892b551669df31b6eb6c897b9
ruleline <ruleline@outlook.com>
2025-02-18 23:55:44 +0800

    file📦: [platform][eth]增加初始文件

---
+++ source/required/platform/eth/eth.h
@@ -0,0 +1,26 @@
+/**
+ * @file eth.h
+ * @brief eth
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined ETH_H
+#define ETH_H
+
+
+
+#endif /* !defined ETH_H */
