c7be15ad848486f3eddf55c0dece94cb07b533c1
ruleline <ruleline@outlook.com>
2025-02-18 23:53:05 +0800

    file📦: [platform][spi]增加初始文件

---
+++ source/required/platform/spi/spi.h
@@ -0,0 +1,26 @@
+/**
+ * @file spi.h
+ * @brief spi
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined SPI_H
+#define SPI_H
+
+
+
+#endif /* !defined SPI_H */
