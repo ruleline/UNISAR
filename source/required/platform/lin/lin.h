fba1db9d791cb89ce447883ccddd043a6172be6c
ruleline <ruleline@outlook.com>
2025-02-18 23:56:30 +0800

    file📦: [platform][lin]增加初始文件

---
+++ source/required/platform/lin/lin.h
@@ -0,0 +1,26 @@
+/**
+ * @file lin.h
+ * @brief lin
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined LIN_H
+#define LIN_H
+
+
+
+#endif /* !defined LIN_H */
