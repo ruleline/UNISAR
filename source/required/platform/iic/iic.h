1efc59dabf988e58ebded0be5034d730abac871e
ruleline <ruleline@outlook.com>
2025-02-18 23:54:01 +0800

    file📦: [platform][iic]增加初始文件

---
+++ source/required/platform/iic/iic.h
@@ -0,0 +1,26 @@
+/**
+ * @file iic.h
+ * @brief iic
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-18
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-18
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined IIC_H
+#define IIC_H
+
+
+
+#endif /* !defined IIC_H */
