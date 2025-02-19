dcd97062bfbaa5d476811555d2a0906a4de66b85
ruleline <ruleline@outlook.com>
2025-02-19 19:06:05 +0800

    feat✨: [config]增加item优先级

--- source/required/unisis/config.h
+++ source/required/unisis/config.h
@@ -31,4 +31,39 @@
 #define PRINTF          printf /**< 常规打印, 需默认加上头(file&line)、尾(换行符) */
 #define PRINT           printf /**< 立即打印, 需默认加上头(file&line)、尾(换行符) */
 
+enum ITEM_PRIORITY {
+        MINIMUM_UNREACHABLE_PRIORITY = 100,
+        CAN1_PRIORITY,
+        CAN2_PRIORITY,
+        CAN3_PRIORITY,
+        CAN4_PRIORITY,
+        CAN5_PRIORITY,
+        ETH1_PRIORITY,
+        ETH2_PRIORITY,
+        ETH3_PRIORITY,
+        ETH4_PRIORITY,
+        ETH5_PRIORITY,
+        UART1_PRIORITY,
+        UART2_PRIORITY,
+        UART3_PRIORITY,
+        UART4_PRIORITY,
+        UART5_PRIORITY,
+        SPI1_PRIORITY,
+        SPI2_PRIORITY,
+        SPI3_PRIORITY,
+        SPI4_PRIORITY,
+        SPI5_PRIORITY,
+        IIC1_PRIORITY,
+        IIC2_PRIORITY,
+        IIC3_PRIORITY,
+        IIC4_PRIORITY,
+        IIC5_PRIORITY,
+        LIN1_PRIORITY,
+        LIN2_PRIORITY,
+        LIN3_PRIORITY,
+        LIN4_PRIORITY,
+        LIN5_PRIORITY,
+        MAXIMUM_UNREACHABLE_PRIORITY = 65536,
+};
+
 #endif /* !defined CONFIG_H */
