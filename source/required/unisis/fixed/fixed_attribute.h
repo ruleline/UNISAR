4b1cec01b7f7846a57f82a95863893df358a553b
ruleline <ruleline@outlook.com>
2025-02-20 21:03:50 +0800

    perf👌: [unisis][config]修改attribute

--- source/required/unisis/fixed/fixed_attribute.h
+++ source/required/unisis/fixed/fixed_attribute.h
@@ -5,7 +5,7 @@
  * @since 2025-02-19
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-19
+ * @date 2025-02-20
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -25,7 +25,7 @@
 #define __unused        __attribute__((__unused__))
 #define __packed        __attribute__((__packed__))
 #define __aligned(x)    __attribute__((aligned(x)))
-#define __always_inline __attribute__((__always_inline__))
+#define __force_inline  __attribute__((__always_inline__))
 #define __section(x)    __attribute__((__section__(x)))
 #define __first         __attribute__((constructor))    /**< 早于__ctor(x)执行 */
 #define __last          __attribute__((destructor))     /**< 晚于__dtor(x)执行 */
