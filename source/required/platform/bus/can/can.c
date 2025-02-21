dcd45fb8d969778c7129c345bdb969b592ac5389
ruleline <ruleline@outlook.com>
2025-02-21 20:28:01 +0800

    feat✨: [platform][bus][can]增加init/deinit/create/send/receive

--- source/required/platform/bus/can/can.c
+++ source/required/platform/bus/can/can.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-21
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -19,3 +19,241 @@
  */
 
 #include "can.h"
+
+/**
+ * @brief 定义一个包含 5 个 CAN 对象的静态数组。
+ * @details 可用于表示系统中的 5 个不同 CAN 对象.
+ *          每个对象可以独立配置和操作, 以满足不同的 CAN 通信需求.
+ */
+static struct CAN can[5];
+
+/**
+ * @defgroup CANInitialization CAN 设备初始化
+ * @brief 包含用于初始化 CAN 设备的函数.
+ * @details 在系统启动阶段被调用, 用于对 CAN 设备进行必要的配置和初始化操作.
+ */
+
+/**
+ * @brief 以 CAN1 的优先级执行初始化操作.
+ * @details 使用 __ctor 机制, 会在程序启动阶段按照 CAN1_PRIORITY 指定的优先级执行初始化操作.
+ *          主要用于对 CAN1 设备进行初始化, 可能包括设置波特率、配置寄存器、启用中断等操作.
+ * @ingroup CANInitialization
+ */
+static __ctor(CAN1_PRIORITY) void init1(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN2 的优先级执行初始化操作.
+ * @details 使用 __ctor 机制, 会在程序启动阶段按照 CAN2_PRIORITY 指定的优先级执行初始化操作.
+ *          主要用于对 CAN2 设备进行初始化, 可能包括设置波特率、配置寄存器、启用中断等操作.
+ * @ingroup CANInitialization
+ */
+static __ctor(CAN2_PRIORITY) void init2(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN3 的优先级执行初始化操作.
+ * @details 使用 __ctor 机制, 会在程序启动阶段按照 CAN3_PRIORITY 指定的优先级执行初始化操作.
+ *          主要用于对 CAN3 设备进行初始化, 可能包括设置波特率、配置寄存器、启用中断等操作.
+ * @ingroup CANInitialization
+ */
+static __ctor(CAN3_PRIORITY) void init3(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN4 的优先级执行初始化操作.
+ * @details 使用 __ctor 机制, 会在程序启动阶段按照 CAN4_PRIORITY 指定的优先级执行初始化操作.
+ *          主要用于对 CAN4 设备进行初始化, 可能包括设置波特率、配置寄存器、启用中断等操作.
+ * @ingroup CANInitialization
+ */
+static __ctor(CAN4_PRIORITY) void init4(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN5 的优先级执行初始化操作.
+ * @details 使用 __ctor 机制, 会在程序启动阶段按照 CAN5_PRIORITY 指定的优先级执行初始化操作.
+ *          主要用于对 CAN5 设备进行初始化, 可能包括设置波特率、配置寄存器、启用中断等操作.
+ * @ingroup CANInitialization
+ */
+static __ctor(CAN5_PRIORITY) void init5(void)
+{
+        /* TODO */
+}
+
+/**
+ * @defgroup CANDeinitialization CAN 设备反初始化
+ * @brief 包含用于对 CAN 设备进行反初始化操作的函数.
+ * @details 在系统关闭或不再需要使用 CAN 设备时被调用, 用于释放资源、恢复默认设置等.
+ */
+
+/**
+ * @brief 以 CAN1 的优先级执行反初始化操作.
+ * @details 利用 __dtor 机制, 会在程序结束阶段按照 CAN1_PRIORITY 指定的优先级执行反初始化操作.
+ *          主要用于对 CAN1 设备进行反初始化, 可能包括停止数据传输、关闭相关中断、释放占用的资源等操作.
+ * @ingroup CANDeinitialization
+ */
+static __dtor(CAN1_PRIORITY) void deinit1(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN2 的优先级执行反初始化操作.
+ * @details 利用 __dtor 机制, 会在程序结束阶段按照 CAN2_PRIORITY 指定的优先级执行反初始化操作.
+ *          主要用于对 CAN2 设备进行反初始化, 可能包括停止数据传输、关闭相关中断、释放占用的资源等操作.
+ * @ingroup CANDeinitialization
+ */
+static __dtor(CAN2_PRIORITY) void deinit2(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN3 的优先级执行反初始化操作.
+ * @details 利用 __dtor 机制, 会在程序结束阶段按照 CAN3_PRIORITY 指定的优先级执行反初始化操作.
+ *          主要用于对 CAN3 设备进行反初始化, 可能包括停止数据传输、关闭相关中断、释放占用的资源等操作.
+ * @ingroup CANDeinitialization
+ */
+static __dtor(CAN3_PRIORITY) void deinit3(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN4 的优先级执行反初始化操作.
+ * @details 利用 __dtor 机制, 会在程序结束阶段按照 CAN4_PRIORITY 指定的优先级执行反初始化操作.
+ *          主要用于对 CAN4 设备进行反初始化, 可能包括停止数据传输、关闭相关中断、释放占用的资源等操作.
+ * @ingroup CANDeinitialization
+ */
+static __dtor(CAN4_PRIORITY) void deinit4(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 以 CAN5 的优先级执行反初始化操作.
+ * @details 利用 __dtor 机制, 会在程序结束阶段按照 CAN5_PRIORITY 指定的优先级执行反初始化操作.
+ *          主要用于对 CAN5 设备进行反初始化, 可能包括停止数据传输、关闭相关中断、释放占用的资源等操作.
+ * @ingroup CANDeinitialization
+ */
+static __dtor(CAN5_PRIORITY) void deinit5(void)
+{
+        /* TODO */
+}
+
+/**
+ * @brief 发送经典 CAN 报文
+ * @param[in] self CAN 对象
+ * @param[in] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static i32 send_classic_(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        ASSERT(self->type == CLASSIC_CAN);
+        ASSERT(package);
+        ASSERT(package->length);
+        ASSERT(package->length <= 8);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+/**
+ * @brief 接收经典 CAN 报文
+ * @param[in] self CAN 对象
+ * @param[out] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static i32 receive_classic_(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        ASSERT(package);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+/**
+ * @brief 发送 CANFD 报文
+ * @param[in] self CAN 对象
+ * @param[in] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static i32 send_flexible_(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        ASSERT(self->type == FLEXIBLE_CAN);
+        ASSERT(package);
+        ASSERT(package->length);
+        ASSERT(package->length <= 64);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+/**
+ * @brief 接收 CANFD 报文
+ * @param[in] self CAN 对象
+ * @param[out] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static i32 receive_flexible_(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        ASSERT(package);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+
+/**
+ * @brief 创建一个 CAN 对象
+ * @details 该函数用于初始化一个 CAN 对象, 并为其指定名称.
+ * @param[out] self CAN 对象
+ * @param[in] name 名称
+ * @param[in] type 类型
+ * @return 结果
+ * @retval -1 CAN 对象已满
+ * @retval 0 成功
+ */
+i32 can_create(struct CAN *self, char *name, u8 type)
+{
+        ASSERT(self);
+        ASSERT(name);
+        ASSERT(strlen(name));
+        ASSERT(strlen(name) < sizeof(self->name));
+        ASSERT(type < EXTENDED_CAN);
+
+        for (u8 i = 0; i < ARRAY_SIZE(can); i++) {
+                if (can[i].name[0]) {
+                        continue;
+                }
+                strcpy(&can[i].name[0], name);
+                can[i].type = type;
+                if (type == CLASSIC_CAN) {
+                        can[i].send = send_classic_;
+                        can[i].receive = receive_classic_;
+                } else {
+                        can[i].send = send_flexible_;
+                        can[i].receive = receive_flexible_;
+                }
+                self = &can[i];
+                PRINTF("[CAN] create %s success.", self->name);
+                return 0;
+        }
+        return -1;
+}
--- source/required/platform/bus/can/can.h
+++ source/required/platform/bus/can/can.h
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-21
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -21,6 +21,129 @@
 #if !defined CAN_H
 #define CAN_H
 
+#include "unisis.h"
 
+/**
+ * @enum CAN_TYPE
+ * @brief 定义不同类型的 CAN (Controller Area Network).
+ * @details 此枚举列出了几种常见的 CAN 类型，用于区分不同的 CAN 总线协议或特性.
+ */
+enum CAN_TYPE {
+        /**
+         * @brief 经典 CAN
+         * @details 代表经典 CAN 协议, 具有基本的功能和数据传输能力.
+         */
+        CLASSIC_CAN,
+        /**
+         * @brief CANFD
+         * @details 这种类型的 CAN 具备更灵活的配置选项和更高的性能.
+         */
+        FLEXIBLE_CAN,
+        /**
+         * @brief CANXL
+         * @details 扩展 CAN 通常支持更多的功能和更大的数据传输量.
+         */
+        EXTENDED_CAN,
+};
+
+/**
+ * @struct CAN_PACKAGE
+ * @brief CAN 数据包
+ * @details 用于封装 CAN 数据包的数据及其长度.
+ */
+struct CAN_PACKAGE {
+        /**
+         * @var CAN_PACKAGE::data
+         * @brief 数据
+         * @details 用于存储 CAN 数据包的实际数据.
+         */
+        u8 *data;
+
+        /**
+         * @var CAN_PACKAGE::length
+         * @brief 数据长度
+         * @details 用于存储 CAN 数据包的实际数据长度.
+         */
+        usize length;
+};
+
+/**
+ * @struct CAN
+ * @brief CAN 对象
+ * @details 此结构体用于封装 CAN 对象的相关信息和操作函数,
+ *          方便对 CAN 对象进行统一管理和操作.
+ */
+struct CAN {
+        /**
+         * @var CAN::name
+         * @brief 名称
+         * @details 用于存储 CAN 对象的名称, 方便在系统中识别和区分不同的 CAN 对象.
+         */
+        char name[20];
+
+        /**
+         * @var CAN::type
+         * @brief 类型
+         * @details 用于表示 CAN 对象的类型, 不同的类型可能对应不同的 CAN 协议版本或硬件特性.
+         */
+        u8 type;
+
+        /**
+         * @var CAN::send
+         * @brief 发送
+         * @details 用于通过 CAN 对象发送数据包的函数.
+         * @param[in] self CAN 对象
+         * @param[in] package 数据包
+         * @return 结果
+         * @retval 0 成功
+         */
+        i32 (*send)(struct CAN *self, struct CAN_PACKAGE *package);
+
+        /**
+         * @var CAN::receive
+         * @brief 接收
+         * @details 用于从 CAN 对象接收数据包的函数.
+         * @param[in] self CAN 对象
+         * @param[out] package 数据包
+         * @return 结果
+         * @retval 0 成功
+         */
+        i32 (*receive)(struct CAN *self, struct CAN_PACKAGE *package);
+};
+
+/**
+ * @brief 发送
+ * @param[in] self CAN 对象
+ * @param[in] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static inline i32 can_send(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        return self->send(self, package);
+}
+
+/**
+ * @brief 接收
+ * @param[in] self CAN 对象
+ * @param[out] package 数据包
+ * @return 结果
+ * @retval 0 成功
+ */
+static inline i32 can_receive(struct CAN *self, struct CAN_PACKAGE *package)
+{
+        return self->receive(self, package);
+}
+
+/**
+ * @brief 创建一个 CAN 对象
+ * @details 该函数用于初始化一个 CAN 对象, 并为其指定名称.
+ * @param[out] self CAN 对象
+ * @param[in] name 名称
+ * @param[in] type 类型
+ * @return 结果
+ * @retval 0 成功
+ */
+i32 can_create(struct CAN *self, char *name, u8 type);
 
 #endif /* !defined CAN_H */
