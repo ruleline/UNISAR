a3bf9350f90723182a7c8cd8f6376820ace4b6fb
ruleline <ruleline@outlook.com>
2025-02-21 22:23:53 +0800

    feat✨: [platform][bus][spi]增加init/deinit/create/send/receive

--- source/required/platform/bus/spi/spi.c
+++ source/required/platform/bus/spi/spi.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-21
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -19,3 +19,125 @@
  */
 
 #include "spi.h"
+
+static struct SPI spi[5];
+
+static __ctor(SPI1_PRIORITY) void init1(void)
+{
+        /* TODO */
+}
+
+static __ctor(SPI2_PRIORITY) void init2(void)
+{
+        /* TODO */
+}
+
+static __ctor(SPI3_PRIORITY) void init3(void)
+{
+        /* TODO */
+}
+
+static __ctor(SPI4_PRIORITY) void init4(void)
+{
+        /* TODO */
+}
+
+static __ctor(SPI5_PRIORITY) void init5(void)
+{
+        /* TODO */
+}
+
+static __dtor(SPI1_PRIORITY) void deinit1(void)
+{
+        /* TODO */
+}
+
+static __dtor(SPI2_PRIORITY) void deinit2(void)
+{
+        /* TODO */
+}
+
+static __dtor(SPI3_PRIORITY) void deinit3(void)
+{
+        /* TODO */
+}
+
+static __dtor(SPI4_PRIORITY) void deinit4(void)
+{
+        /* TODO */
+}
+
+static __dtor(SPI5_PRIORITY) void deinit5(void)
+{
+        /* TODO */
+}
+
+static i32 send_spi_(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        ASSERT(self->type == SPI_COM);
+        ASSERT(package);
+        ASSERT(package->length);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+static i32 send_qspi_(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        ASSERT(self->type == QSPI_COM);
+        ASSERT(package);
+        ASSERT(package->length);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+static i32 receive_spi_(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        ASSERT(self->type == SPI_COM);
+        ASSERT(package);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+static i32 receive_qspi_(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        ASSERT(self->type == QSPI_COM);
+        ASSERT(package);
+        ASSERT(package->data);
+
+        /* TODO */
+        return 0;
+}
+
+i32 spi_create(struct SPI *self, char *name, u8 type)
+{
+        ASSERT(self);
+        ASSERT(name);
+        ASSERT(strlen(name));
+        ASSERT(strlen(name) < sizeof(self->name));
+        ASSERT((type == SPI_COM) || (type == QSPI_COM));
+
+        for (u8 i = 0; i < ARRAY_SIZE(spi); i++) {
+                if (spi[i].name[0]) {
+                        continue;
+                }
+                strcpy(&spi[i].name[0], name);
+                spi[i].type = type;
+                if (type == SPI_COM) {
+                        spi[i].send = send_spi_;
+                        spi[i].receive = receive_spi_;
+                } else {
+                        spi[i].send = send_qspi_;
+                        spi[i].receive = receive_qspi_;
+                }
+                self = &spi[i];
+                PRINTF("[SPI] create %s success.", self->name);
+                return 0;
+        }
+        return -1;
+}
--- source/required/platform/bus/spi/spi.h
+++ source/required/platform/bus/spi/spi.h
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-21
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -21,6 +21,35 @@
 #if !defined SPI_H
 #define SPI_H
 
+#include "unisis.h"
 
+enum SPI_TYPE {
+        SPI_COM,
+        QSPI_COM,
+};
+
+struct SPI_PACKAGE {
+        u8 *data;
+        usize length;
+};
+
+struct SPI {
+        char *name;
+        u8 type;
+        i32 (*send)(struct SPI *self, struct SPI_PACKAGE *package);
+        i32 (*receive)(struct SPI *self, struct SPI_PACKAGE *package);
+};
+
+static i32 spi_send(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        return self->send(self, package);
+}
+
+static i32 spi_receive(struct SPI *self, struct SPI_PACKAGE *package)
+{
+        return self->receive(self, package);
+}
+
+i32 spi_create(struct SPI *self, char *name, u8 type);
 
 #endif /* !defined SPI_H */
