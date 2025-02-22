acf5b2241cb1c09b517fb5d72d4477a20b7c9581
ruleline <ruleline@outlook.com>
2025-02-22 23:30:34 +0800

    feat✨: [platform][gpio]增加init/deinit/read/write/toggle/highz/create

--- source/required/platform/gpio/gpio.c
+++ source/required/platform/gpio/gpio.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-22
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -19,3 +19,103 @@
  */
 
 #include "gpio.h"
+
+static struct GPIO gpio[5];
+
+static __ctor(GPIO1_PRIORITY) void init1(void)
+{
+        /* TODO */
+}
+
+static __ctor(GPIO2_PRIORITY) void init2(void)
+{
+        /* TODO */
+}
+
+static __ctor(GPIO3_PRIORITY) void init3(void)
+{
+        /* TODO */
+}
+
+static __ctor(GPIO4_PRIORITY) void init4(void)
+{
+        /* TODO */
+}
+
+static __ctor(GPIO5_PRIORITY) void init5(void)
+{
+        /* TODO */
+}
+
+static __dtor(GPIO1_PRIORITY) void deinit1(void)
+{
+        /* TODO */
+}
+
+static __dtor(GPIO2_PRIORITY) void deinit2(void)
+{
+        /* TODO */
+}
+static __dtor(GPIO3_PRIORITY) void deinit3(void)
+{
+        /* TODO */
+}
+
+static __dtor(GPIO4_PRIORITY) void deinit4(void)
+{
+        /* TODO */
+}
+
+static __dtor(GPIO5_PRIORITY) void deinit5(void)
+{
+        /* TODO */
+}
+
+static bool read_(struct GPIO *self)
+{
+        bool state = 0;
+
+        /* TODO */
+        return state;
+}
+
+static i32 write_(struct GPIO *self, bool state)
+{
+        /* TODO */
+        return 0;
+}
+
+static i32 toggle_(struct GPIO *self)
+{
+        /* TODO */
+        return 0;
+}
+
+static i32 highz_(struct GPIO *self)
+{
+        /* TODO */
+        return 0;
+}
+
+i32 gpio_create(struct GPIO *self, char *name)
+{
+        ASSERT(self);
+        ASSERT(name);
+        ASSERT(strlen(name));
+        ASSERT(strlen(name) < sizeof(self->name));
+
+        for (u8 i = 0; i < ARRAY_SIZE(gpio); i++) {
+                if (gpio[i].name[0]) {
+                        continue;
+                }
+                strcpy(&gpio[i].name[0], name);
+                gpio[i].read = read_;
+                gpio[i].write = write_;
+                gpio[i].toggle = toggle_;
+                gpio[i].highz = highz_;
+                self = &gpio[i];
+                PRINTF("[GPIO] create %s success.", self->name);
+                return 0;
+        }
+        return -1;
+}
--- source/required/platform/gpio/gpio.h
+++ source/required/platform/gpio/gpio.h
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-18
+ * @date 2025-02-22
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -21,6 +21,36 @@
 #if !defined GPIO_H
 #define GPIO_H
 
+#include "unisis.h"
 
+struct GPIO {
+        char name[20];
+        bool (*read)(struct GPIO *gpio);
+        i32 (*write)(struct GPIO *gpio, bool state);
+        i32 (*toggle)(struct GPIO *gpio);
+        i32 (*highz)(struct GPIO *gpio);
+};
+
+static __force_inline bool gpio_read(struct GPIO *self)
+{
+        return self->read(self);
+}
+
+static __force_inline i32 gpio_write(struct GPIO *self, bool state)
+{
+        return self->write(self, state);
+}
+
+static __force_inline i32 gpio_toggle(struct GPIO *self)
+{
+        return self->toggle(self);
+}
+
+static __force_inline i32 gpio_highz(struct GPIO *self)
+{
+        return self->highz(self);
+}
+
+i32 gpio_create(struct GPIO *self, char *name);
 
 #endif /* !defined GPIO_H */
