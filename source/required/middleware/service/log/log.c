f0e181822450da76f18405093d84fc84dc778d41
ruleline <ruleline@outlook.com>
2025-02-24 21:13:26 +0800

    file📦: [middleware][service]增加log

---
+++ source/required/middleware/service/log/log.c
@@ -0,0 +1,21 @@
+/**
+ * @file log.c
+ * @brief log
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-24
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-24
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-24 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "log.h"
---
+++ source/required/middleware/service/log/log.h
@@ -0,0 +1,26 @@
+/**
+ * @file log.h
+ * @brief log
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-24
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-24
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-24 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined LOG_H
+#define LOG_H
+
+
+
+#endif /* !defined LOG_H */
