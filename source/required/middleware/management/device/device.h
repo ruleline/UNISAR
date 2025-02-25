a24e8caf317ef1baddcace7fa388cec233d7c2bb
ruleline <ruleline@outlook.com>
2025-02-25 22:46:56 +0800

    file📦: [middleware][management]增加device模块

---
+++ source/required/middleware/management/device/device.h
@@ -0,0 +1,26 @@
+/**
+ * @file device.h
+ * @brief device
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined DEVICE_H
+#define DEVICE_H
+
+
+
+#endif /* !defined DEVICE_H */
