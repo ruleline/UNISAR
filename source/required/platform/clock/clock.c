b38a6ff6b3cf99a3e77731fe89617b6b95876676
ruleline <ruleline@outlook.com>
2025-02-25 22:38:58 +0800

    file📦: [platform]增加clock模块

---
+++ source/required/platform/clock/clock.c
@@ -0,0 +1,21 @@
+/**
+ * @file clock.c
+ * @brief clock
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "clock.h"
---
+++ source/required/platform/clock/clock.h
@@ -0,0 +1,26 @@
+/**
+ * @file clock.h
+ * @brief clock
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined CLOCK_H
+#define CLOCK_H
+
+
+
+#endif /* !defined CLOCK_H */
