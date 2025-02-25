05c63d14976fbaf67947a40529bb82cbe8d981ef
ruleline <ruleline@outlook.com>
2025-02-25 22:40:43 +0800

    file📦: [platform]增加watchdog模块

---
+++ source/required/platform/watchdog/watchdog.c
@@ -0,0 +1,21 @@
+/**
+ * @file watchdog.c
+ * @brief watchdog
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "watchdog.h"
---
+++ source/required/platform/watchdog/watchdog.h
@@ -0,0 +1,26 @@
+/**
+ * @file watchdog.h
+ * @brief watchdog
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined WATCHDOG_H
+#define WATCHDOG_H
+
+
+
+#endif /* !defined WATCHDOG_H */
