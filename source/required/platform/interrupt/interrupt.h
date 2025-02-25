99633103b5af34444c03f5b277938e7370fcbf7a
ruleline <ruleline@outlook.com>
2025-02-25 22:39:54 +0800

    file📦: [platform]增加interrupt模块

---
+++ source/required/platform/interrupt/interrupt.h
@@ -0,0 +1,26 @@
+/**
+ * @file interrupt.h
+ * @brief interrupt
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-25
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-25
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined INTERRUPT_H
+#define INTERRUPT_H
+
+
+
+#endif /* !defined INTERRUPT_H */
