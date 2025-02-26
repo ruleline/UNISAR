cdf052bf891299dd09e7508e0da11f241474f8d7
ruleline <ruleline@outlook.com>
2025-02-26 21:29:35 +0800

    feat✨: [unisis][object]增加name/open/write/read/close方法

--- source/required/unisis/object/object.h
+++ source/required/unisis/object/object.h
@@ -21,6 +21,39 @@
 #if !defined OBJECT_H
 #define OBJECT_H
 
+#include "unisis.h"
 
+struct OBJECT {
+        char *name;
+        i32 (*open)(struct OBJECT *self);
+        i32 (*close)(struct OBJECT *self);
+        i32 (*read)(struct OBJECT *self, void *package);
+        i32 (*write)(struct OBJECT *self, void *package);
+};
+
+static __force_inline char *object_name_(struct OBJECT *self)
+{
+        return (self->name);
+}
+
+static __force_inline i32 object_open_(struct OBJECT *self)
+{
+        return (self->open(self));
+}
+
+static __force_inline i32 object_close_(struct OBJECT *self)
+{
+        return (self->close(self));
+}
+
+static __force_inline i32 object_read_(struct OBJECT *self, void *package)
+{
+        return (self->read(self, package));
+}
+
+static __force_inline i32 object_write_(struct OBJECT *self, void *package)
+{
+        return (self->write(self, package));
+}
 
 #endif /* !defined OBJECT_H */
