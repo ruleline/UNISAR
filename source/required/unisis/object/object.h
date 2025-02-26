5d9b00cd9c20c14fd6e2ad1159bfd24144cfdd70
ruleline <ruleline@outlook.com>
2025-02-26 21:13:55 +0800

    file📦: [unisis]增加object

---
+++ source/required/unisis/object/object.h
@@ -0,0 +1,26 @@
+/**
+ * @file object.h
+ * @brief object
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-02-26
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-02-26
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-02-26 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined OBJECT_H
+#define OBJECT_H
+
+
+
+#endif /* !defined OBJECT_H */
