637ac8ce78ea5fb0cd1720a45280c7eaf1c2443a
ruleline <ruleline@outlook.com>
2025-03-03 20:50:53 +0800

    file📦: [platform]增加file模块

---
+++ source/required/platform/file/file.c
@@ -0,0 +1,21 @@
+/**
+ * @file file.c
+ * @brief file
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-03-03
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-03-03
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-03-03 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#include "file.h"
---
+++ source/required/platform/file/file.h
@@ -0,0 +1,26 @@
+/**
+ * @file file.h
+ * @brief file
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-03-03
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-03-03
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-03-03 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined FILE_H
+#define FILE_H
+
+
+
+#endif /* !defined FILE_H */
