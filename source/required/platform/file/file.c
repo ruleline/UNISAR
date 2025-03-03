b571e27dff9ce2a5f0609fc2494f93d7eeb001fb
ruleline <ruleline@outlook.com>
2025-03-03 21:04:24 +0800

    feat✨: [platform][file]增加create/open/close/read/write/erase接口

--- source/required/platform/file/file.c
+++ source/required/platform/file/file.c
@@ -19,3 +19,70 @@
  */
 
 #include "file.h"
+
+static struct FILE file[FILE_MAX];
+
+static __force_inline i32 open_(struct FILE *self)
+{
+        /* TODO */
+        return (0);
+}
+
+static __force_inline i32 close_(struct FILE *self)
+{
+        /* TODO */
+        return (0);
+}
+
+static __force_inline i32 read_(struct FILE *self, struct FILE_PACKAGE *package)
+{
+        /* TODO */
+        return (0);
+}
+
+static __force_inline i32 write_(struct FILE *self, struct FILE_PACKAGE *package)
+{
+        /* TODO */
+        return (0);
+}
+
+static __force_inline i32 erase_(struct FILE *self)
+{
+        /* TODO */
+        return (0);
+}
+
+static __ctor(FILE1_PRIORITY) void init1_(void)
+{
+        struct FILE *self = &file[FILE_ALL];
+        static char *name = "file-all";
+        static struct OBJECT super;
+
+        /* TODO */
+        super.name = name;
+        super.open = &open_;
+        super.close = &close_;
+        super.read = &read_;
+        super.write = &write_;
+        self->super = &super;
+        self->erase = &erase_;
+        PRINTF("[FILE] init %s successfully", file_name_(self));
+}
+
+static __dtor(FILE1_PRIORITY) void deinit1_(void)
+{
+        struct FILE *self = &file[FILE_ALL];
+
+        /* TODO */
+        PRINTF("[FILE] deinit %s successfully", file_name_(self));
+}
+
+i32 file_create(struct FILE *self, u8 id)
+{
+        ASSERT(self);
+        ASSERT(id < ARRAY_SIZE(file));
+
+        self = &file[id];
+        PRINTF("[FILE] create %s successfully", file_name_(self));
+        return (0);
+}
--- source/required/platform/file/file.h
+++ source/required/platform/file/file.h
@@ -21,6 +21,55 @@
 #if !defined FILE_H
 #define FILE_H
 
+#include "unisis.h"
 
+enum FILE_ID {
+        FILE_ALL,
+        FILE_MAX,
+};
+
+struct FILE_PACKAGE {
+        u8 *data;
+        usize length;
+};
+
+struct FILE {
+        struct OBJECT *super;
+        i32 (*erase)(struct FILE *self);
+};
+
+static __force_inline char *file_name_(struct FILE *self)
+{
+        return (object_name_(self));
+}
+
+static __force_inline i32 file_open_(struct FILE *self)
+{
+        return (object_open_(self));
+}
+
+static __force_inline i32 file_close_(struct FILE *self)
+{
+        return (object_close_(self));
+}
+
+static __force_inline i32 file_read_(struct FILE *self,
+                                        struct FILE_PACKAGE *package)
+{
+        return (object_read_(self, package));
+}
+
+static __force_inline i32 file_write_(struct FILE *self,
+                                        struct FILE_PACKAGE *package)
+{
+        return (object_write_(self, package));
+}
+
+static __force_inline i32 file_erase_(struct FILE *self)
+{
+        return (self->erase(self));
+}
+
+i32 file_create(struct FILE *self, u8 id);
 
 #endif /* !defined FILE_H */
