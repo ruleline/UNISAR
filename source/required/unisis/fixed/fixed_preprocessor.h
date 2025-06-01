a936f707ac08dd790c552400018117fd725e01fb
ruleline <ruleline@outlook.com>
2025-06-01 19:01:29 +0800

    perf👌: update comments in fixed_preprocessor.h to English

--- source/required/unisis/fixed/fixed_preprocessor.h
+++ source/required/unisis/fixed/fixed_preprocessor.h
@@ -5,19 +5,19 @@
  * @since 2025-02-21
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-21
+ * @date 2025-06-01
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
  *
- * @ref 1. C/C++ 预处理器参考
+ * @ref 1. C/C++ preprocessor directives
  *         https://learn.microsoft.com/zh-cn/cpp/preprocessor
  *
  * @details
  * -----------------------------------------------------------------------------
  *    version   |    date    |     author     |             comments
  * ------------ | ---------- | -------------- | --------------------------------
- *   0.00.001   | 2025-02-21 |    ruleline    | 初版
+ *   0.00.001   | 2025-02-21 |    ruleline    | initial commit
  * -----------------------------------------------------------------------------
  */
 
@@ -25,6 +25,12 @@
 #define FIXED_PREPROCESSOR_H
 
 #if __INTELLISENSE__
+/**
+ * @brief suppress the warning about the use of deprecated preprocessor directives in IntelliSense.
+ * @details
+ * this is a workaround for IntelliSense to avoid warnings about deprecated
+ * preprocessor directives.
+ */
 #pragma diag_suppress 1094
 #endif /* __INTELLISENSE__ */
 
