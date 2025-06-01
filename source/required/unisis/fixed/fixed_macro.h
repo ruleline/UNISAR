7d0bbdf65e2eec4184dc4b6b180a4d4bc5ff2b7f
ruleline <ruleline@outlook.com>
2025-06-01 18:58:15 +0800

    perf👌: update comments in fixed_macro.h to English

--- source/required/unisis/fixed/fixed_macro.h
+++ source/required/unisis/fixed/fixed_macro.h
@@ -5,7 +5,7 @@
  * @since 2025-02-19
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-19
+ * @date 2025-06-01
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -14,7 +14,7 @@
  * -----------------------------------------------------------------------------
  *    version   |    date    |     author     |             comments
  * ------------ | ---------- | -------------- | --------------------------------
- *   0.00.001   | 2025-02-19 |    ruleline    | 初版
+ *   0.00.001   | 2025-02-19 |    ruleline    | initial commit
  * -----------------------------------------------------------------------------
  */
 
@@ -22,6 +22,14 @@
 #define FIXED_MACRO_H
 
 #if !defined ASSERT
+/**
+ * @brief assert a condition and print an error message if the condition is false.
+ * @details
+ * this macro can be used to assert a condition and print an error message if the
+ * condition is false. the error message is formatted using the printf-style
+ * format string and arguments.
+ * @param[in] x the condition to assert.
+ */
 #define ASSERT(x)                               \
 do {                                            \
         if (!(x)) {                             \
@@ -32,6 +40,13 @@ do {                                            \
 #endif /* !defined ASSERT */
 
 #if !defined ARRAY_SIZE
+/**
+ * @brief get the size of an array.
+ * @details
+ * this macro can be used to get the size of an array. the array size is calculated
+ * as the size of the array divided by the size of the first element in the array.
+ * @param[in] x the array to get the size of.
+ */
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #endif /*!defined ARRAY_SIZE */
 
