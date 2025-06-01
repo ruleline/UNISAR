a68046c0432000f4034330b4b20bf25fa6a43ee5
ruleline <ruleline@outlook.com>
2025-06-01 18:25:17 +0800

    file📦: add fixed_operation.h header file

---
+++ source/required/unisis/fixed/fixed_operation.h
@@ -0,0 +1,26 @@
+/**
+ * @file fixed_operation.h
+ * @brief fixed_operation
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-01
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-01
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-01 |    ruleline    | initial commit
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined FIXED_OPERATION_H
+#define FIXED_OPERATION_H
+
+
+
+#endif /* !defined FIXED_OPERATION_H */
