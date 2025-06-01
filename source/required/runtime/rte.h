ab26a8cb8e03cbda7b6bd29d8fd619244d729e96
ruleline <ruleline@outlook.com>
2025-06-01 19:19:38 +0800

    file📦: add rte.h header file

---
+++ source/required/runtime/rte.h
@@ -0,0 +1,26 @@
+/**
+ * @file rte.h
+ * @brief rte
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-01
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-01
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-01 |    ruleline    | initial commit
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined RTE_H
+#define RTE_H
+
+
+
+#endif /* !defined RTE_H */
