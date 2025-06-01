b32d8448169568acc56dd69a417b3e816e0156cc
ruleline <ruleline@outlook.com>
2025-06-02 01:12:42 +0800

    perf👌: enhance comments and function names in uart.c for clarity and consistency.

--- source/required/platform/communication/uart/uart.c
+++ source/required/platform/communication/uart/uart.c
@@ -1,54 +1,91 @@
 /**
  * @file uart.c
- * @brief uart
+ * @brief UART.
  * @author ruleline (ruleline@outlook.com)
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-04-17
+ * @date 2025-06-02
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
- *
- * @defgroup UART-INIT UART 设备初始化
- * @defgroup UART-DEINIT UART 设备反初始化
  *
  * @details
  * -----------------------------------------------------------------------------
  *    version   |    date    |     author     |             comments
  * ------------ | ---------- | -------------- | --------------------------------
- *   0.00.001   | 2025-02-18 |    ruleline    | 初版
+ *   0.00.001   | 2025-02-18 |    ruleline    | initial commit.
  * -----------------------------------------------------------------------------
  */
 
 #include "uart.h"
 
+/**
+ * @brief UART type set.
+ * @details
+ * this enumeration represents the UART type.
+ */
 enum UART_TYPE {
+        /** UART. */
         UART_COM,
+        /** USART. */
         USART_COM,
 };
 
+/**
+ * @brief UART name length.
+ * @details
+ * this enumeration represents the UART name length.
+ */
 enum UART_NAME_LENGTH {
+        /** UART-LOG name length. */
         UART_LOG_NAME_LENGTH = 20,
 };
 
+/**
+ * @brief UART object set.
+ * @details
+ * this structure represents the UART object.
+ */
 static struct UART uart[UART_MAX];
 
-static i32 open_(struct UART *self)
+/**
+ * @brief open the UART.
+ * @details
+ * this function opens the UART.
+ * @param[in,out] self pointer to UART.
+ * @return the result of opening the UART.
+ */
+static i32 open(struct UART *self)
 {
         self->is_open = 1;
-        PRINTF("[UART] open %s successfully", uart_name_(self));
+        PRINTF("[UART] open %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 close_(struct UART *self)
+/**
+ * @brief close the UART.
+ * @details
+ * this function closes the UART.
+ * @param[in,out] self pointer to UART.
+ * @return the result of closing the UART.
+ */
+static i32 close(struct UART *self)
 {
         self->is_open = 0;
-        PRINTF("[UART] close %s successfully", uart_name_(self));
+        PRINTF("[UART] close %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package.
+ * @details
+ * this function sends the package.
+ * @param[in] self pointer to UART.
+ * @param[in] package pointer to UART package.
+ * @return the result of sending the package.
+ */
+static i32 send_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -60,11 +97,19 @@ static i32 send_uart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package.
+ * @details
+ * this function receives the package.
+ * @param[in] self pointer to UART.
+ * @param[out] package pointer to UART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -75,11 +120,19 @@ static i32 receive_uart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package.
+ * @details
+ * this function sends the package.
+ * @param[in] self pointer to USART.
+ * @param[in] package pointer to USART package.
+ * @return the result of sending the package.
+ */
+static i32 send_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -91,11 +144,19 @@ static i32 send_usart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package.
+ * @details
+ * this function receives the package.
+ * @param[in] self pointer to USART.
+ * @param[out] package pointer to USART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -106,11 +167,21 @@ static i32 receive_usart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_blocking_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package by blocking.
+ * @details
+ * this is a blocking operation, meaning it will wait until the package is sent.
+ * it is typically used when the application needs to ensure that the data is sent
+ * before proceeding with further operations.
+ * @param[in] self pointer to UART.
+ * @param[in] package pointer to UART package.
+ * @return the result of sending the package.
+ */
+static i32 send_blocking_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -122,11 +193,21 @@ static i32 send_blocking_uart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_blocking_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package by blocking.
+ * @details
+ * this is a blocking operation, meaning it will wait until the package is
+ * received. it is typically used when the application needs to ensure that the
+ * data is received before proceeding with further operations.
+ * @param[in] self pointer to UART.
+ * @param[out] package pointer to UART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_blocking_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -137,11 +218,21 @@ static i32 receive_blocking_uart_(struct UART *self, struct UART_PACKAGE *packag
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_blocking_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package by blocking.
+ * @details
+ * this is a blocking operation, meaning it will wait until the package is sent.
+ * it is typically used when the application needs to ensure that the data is sent
+ * before proceeding with further operations.
+ * @param[in] self pointer to USART.
+ * @param[in] package pointer to USART package.
+ * @return the result of sending the package.
+ */
+static i32 send_blocking_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -153,11 +244,21 @@ static i32 send_blocking_usart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_blocking_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package by blocking.
+ * @details
+ * this is a blocking operation, meaning it will wait until the package is
+ * received. it is typically used when the application needs to ensure that the
+ * data is received before proceeding with further operations.
+ * @param[in] self pointer to USART.
+ * @param[out] package pointer to USART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_blocking_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -168,11 +269,21 @@ static i32 receive_blocking_usart_(struct UART *self, struct UART_PACKAGE *packa
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_polling_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package by polling.
+ * @details
+ * this is a polling operation, meaning it will check the status of the transmission
+ * until the package is sent. it is typically used when the application needs to
+ * ensure that the data is sent before proceeding with further operations.
+ * @param[in] self pointer to UART.
+ * @param[in] package pointer to UART package.
+ * @return the result of sending the package.
+ */
+static i32 send_polling_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -184,11 +295,21 @@ static i32 send_polling_uart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_polling_uart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package by polling.
+ * @details
+ * this is a polling operation, meaning it will check the status of the reception
+ * until the package is received. it is typically used when the application needs to
+ * ensure that the data is received before proceeding with further operations.
+ * @param[in] self pointer to UART.
+ * @param[out] package pointer to UART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_polling_uart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == UART_COM);
         ASSERT(package);
@@ -199,11 +320,21 @@ static i32 receive_polling_uart_(struct UART *self, struct UART_PACKAGE *package
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 send_polling_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief send the package by polling.
+ * @details
+ * this is a polling operation, meaning it will check the status of the transmission
+ * until the package is sent. it is typically used when the application needs to
+ * ensure that the data is sent before proceeding with further operations.
+ * @param[in] self pointer to USART.
+ * @param[in] package pointer to USART package.
+ * @return the result of sending the package.
+ */
+static i32 send_polling_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -215,11 +346,21 @@ static i32 send_polling_usart_(struct UART *self, struct UART_PACKAGE *package)
         }
 
         /* TODO */
-        PRINTF("[UART] send %s successfully", uart_name_(self));
+        PRINTF("[UART] send %s successfully", uart_name(self));
         return (0);
 }
 
-static i32 receive_polling_usart_(struct UART *self, struct UART_PACKAGE *package)
+/**
+ * @brief receive the package by polling.
+ * @details
+ * this is a polling operation, meaning it will check the status of the reception
+ * until the package is received. it is typically used when the application needs to
+ * ensure that the data is received before proceeding with further operations.
+ * @param[in] self pointer to USART.
+ * @param[out] package pointer to USART package.
+ * @return the result of receiving the package.
+ */
+static i32 receive_polling_usart(struct UART *self, struct UART_PACKAGE *package)
 {
         ASSERT(self->type == USART_COM);
         ASSERT(package);
@@ -230,11 +371,17 @@ static i32 receive_polling_usart_(struct UART *self, struct UART_PACKAGE *packag
         }
 
         /* TODO */
-        PRINTF("[UART] receive %s successfully", uart_name_(self));
+        PRINTF("[UART] receive %s successfully", uart_name(self));
         return (0);
 }
 
-static __ctor(UART1_PRIORITY) void init1_(void)
+/**
+ * @brief initialize UART1.
+ * @details
+ * this function initializes UART1.
+ * @return the result of initializing UART1.
+ */
+static __ctor(UART1_PRIORITY) void init_uart1(void)
 {
         struct UART *self = &uart[UART_LOG];
         static struct OBJECT super;
@@ -242,34 +389,48 @@ static __ctor(UART1_PRIORITY) void init1_(void)
         /* TODO */
 
         super.name = "uart-log";
-        super.open = &open_;
-        super.close = &close_;
-        super.read = &receive_uart_;
-        super.write = &send_uart_;
+        super.open = &open;
+        super.close = &close;
+        super.read = &receive_uart;
+        super.write = &send_uart;
         self->super = &super;
         self->type = UART_COM;
         self->is_open = 0;
-        self->send_blocking = &send_blocking_uart_;
-        self->receive_blocking = &receive_blocking_uart_;
-        self->send_polling = &send_polling_uart_;
-        self->receive_polling = &receive_polling_uart_;
-        PRINTF("[UART] init %s successfully", uart_name_(self));
+        self->send_blocking = &send_blocking_uart;
+        self->receive_blocking = &receive_blocking_uart;
+        self->send_polling = &send_polling_uart;
+        self->receive_polling = &receive_polling_uart;
+        PRINTF("[UART] init %s successfully", uart_name(self));
 }
 
-static __dtor(UART1_PRIORITY) void deinit1_(void)
+/**
+ * @brief deinitialize UART1.
+ * @details
+ * this function deinitializes UART1.
+ * @return the result of deinitializing UART1.
+ */
+static __dtor(UART1_PRIORITY) void deinit_uart1(void)
 {
         struct UART *self = &uart[UART_LOG];
 
         /* TODO */
-        PRINTF("[UART] deinit %s successfully", uart_name_(self));
+        PRINTF("[UART] deinit %s successfully", uart_name(self));
 }
 
+/**
+ * @brief create a UART object.
+ * @details
+ * this function creates a UART object.
+ * @param[in,out] self pointer to UART.
+ * @param[in] id the identifier of UART.
+ * @return the result of creating a UART object.
+ */
 i32 uart_create(struct UART *self, u8 id)
 {
         ASSERT(self);
         ASSERT(id < ARRAY_SIZE(uart));
 
         self = &uart[id];
-        PRINTF("[UART] create %s successfully", uart_name_(self));
+        PRINTF("[UART] create %s successfully", uart_name(self));
         return (0);
 }
