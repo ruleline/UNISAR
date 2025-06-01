83835f1fc94ac258387a7540ab19c1191eba0502
ruleline <ruleline@outlook.com>
2025-06-02 00:15:25 +0800

    file📦: add fixed_status.h file.

---
+++ source/required/unisis/fixed/fixed_status.h
@@ -0,0 +1,26 @@
+/**
+ * @file fixed_status.h
+ * @brief fixed status.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-02
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-02
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-02 |    ruleline    | initial commit
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined FIXED_STATUS_H
+#define FIXED_STATUS_H
+
+
+
+#endif /* !defined FIXED_STATUS_H */
