040ab295370fda141034bdbf32c4e8eea0e9c216
ruleline <ruleline@outlook.com>
2025-06-02 00:33:43 +0800

    feat✨: add enum STATUS with operation status values in fixed_status.h

--- source/required/unisis/fixed/fixed_status.h
+++ source/required/unisis/fixed/fixed_status.h
@@ -21,6 +21,32 @@
 #if !defined FIXED_STATUS_H
 #define FIXED_STATUS_H
 
-
+/**
+ * @brief status.
+ * @details
+ * this enumeration represents the status of an operation.
+ */
+enum STATUS {
+        /** not supported */
+        STATUS_NOT_SUPPORTED = -8,
+        /** not implemented */
+        STATUS_NOT_IMPLEMENTED = -7,
+        /** null pointer */
+        STATUS_NULL_POINTER = -6,
+        /** unknown status */
+        STATUS_UNKNOWN = -5,
+        /** underflow */
+        STATUS_UNDERFLOW = -4,
+        /** overflow */
+        STATUS_OVERFLOW = -3,
+        /** invalid status */
+        STATUS_INVALID = -2,
+        /** error */
+        STATUS_ERROR = -1,
+        /** ok */
+        STATUS_OK = 0,
+        /** busy */
+        STATUS_BUSY = 1,
+};
 
 #endif /* !defined FIXED_STATUS_H */
