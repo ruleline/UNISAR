b6a88a9374fb1c1603ac46d94aa6e590c9c87b34
ruleline <ruleline@outlook.com>
2025-06-04 20:56:21 +0800

    test(optional): add new TEST module.
    - create test.c and test.h files in the optional directory.
    - implement initial version of the TEST module.
    - add version control and copyright information.

---
+++ source/optional/test/test.c
@@ -0,0 +1,21 @@
+/**
+ * @file test.c
+ * @brief TEST module.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-04
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-04
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-04 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#include "test.h"
---
+++ source/optional/test/test.h
@@ -0,0 +1,26 @@
+/**
+ * @file test.h
+ * @brief TEST module.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-04
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-04
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-04 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined TEST_H
+#define TEST_H
+
+
+
+#endif /* !defined TEST_H */
