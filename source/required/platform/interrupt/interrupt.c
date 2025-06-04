a70c758f12fafa355494363682991ced7c97851c
ruleline <ruleline@outlook.com>
2025-06-04 21:16:29 +0800

    feat(interrupt): add system timer support.
    - implement system timer tick and ISR.
    - add get_system_timer_tick function.
    - update interrupt.h with new timer-related declarations.

--- source/required/platform/interrupt/interrupt.c
+++ source/required/platform/interrupt/interrupt.c
@@ -1,11 +1,11 @@
 /**
  * @file interrupt.c
- * @brief interrupt
+ * @brief Interrupt Service Routines (ISRs).
  * @author ruleline (ruleline@outlook.com)
  * @since 2025-02-25
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-02-25
+ * @date 2025-06-04
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -14,8 +14,37 @@
  * -----------------------------------------------------------------------------
  *    version   |    date    |     author     |             comments
  * ------------ | ---------- | -------------- | --------------------------------
- *   0.00.001   | 2025-02-25 |    ruleline    | 初版
+ *   0.00.001   | 2025-02-25 |    ruleline    | initial commit.
  * -----------------------------------------------------------------------------
  */
 
 #include "interrupt.h"
+
+/**
+ * @brief system timer tick.
+ */
+static volatile usize system_timer_tick;
+
+/**
+ * @brief system timer ISR.
+ * @details
+ * this function is called by the system timer interrupt to increment the
+ * system timer tick. it is typically called at regular intervals to keep track
+ * of time in the system.
+ * @return void.
+ */
+void timer_isr(void)
+{
+        system_timer_tick++;
+}
+
+/**
+ * @brief get system timer tick.
+ * @details
+ * this function returns the current value of the system timer tick count.
+ * @return the current system timer tick count.
+ */
+usize get_system_timer_tick(void)
+{
+        return (system_timer_tick);
+}
