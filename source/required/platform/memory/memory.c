1b90024304d5cdc1bb4e35f07c02471b7c9fa47e
ruleline <ruleline@outlook.com>
2025-06-06 21:02:28 +0800

    file(memory): add memory driver interface source and header files.

---
+++ source/required/platform/memory/memory.c
@@ -0,0 +1,21 @@
+/**
+ * @file memory.c
+ * @brief Memory driver interface.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-06
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-06
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-06 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#include "memory.h"
---
+++ source/required/platform/memory/memory.h
@@ -0,0 +1,26 @@
+/**
+ * @file memory.h
+ * @brief Memory driver interface.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-06
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-06
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-06 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined MEMORY_H
+#define MEMORY_H
+
+
+
+#endif /* !defined MEMORY_H */
