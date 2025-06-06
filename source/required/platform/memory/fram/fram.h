4315c6a5b9bd8534b8bf06d61df45a44d9964913
ruleline <ruleline@outlook.com>
2025-06-06 21:00:28 +0800

    file(fram): add FRAM memory driver soource and header files.

---
+++ source/required/platform/memory/fram/fram.h
@@ -0,0 +1,26 @@
+/**
+ * @file fram.h
+ * @brief FRAM memory driver.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-06
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-06
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-06 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined FRAM_H
+#define FRAM_H
+
+
+
+#endif /* !defined FRAM_H */
