bce072fa45ce141999b0c93bc33a6afa8c18c5dc
ruleline <ruleline@outlook.com>
2025-06-09 19:18:34 +0800

    file(object): create object.c file.

---
+++ source/required/unisis/object/object.c
@@ -0,0 +1,21 @@
+/**
+ * @file object.c
+ * @brief OBJECT.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-06-09
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-06-09
+ * @version 0.00.001
+ *
+ * @copyright ©2025 UNISAR
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-06-09 |    ruleline    | initial commit.
+ * -----------------------------------------------------------------------------
+ */
+
+#include "object.h"
