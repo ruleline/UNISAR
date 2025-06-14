63f1aa7d3c8b7f35c986576a085b2d5a3351c4c1
ruleline <ruleline@outlook.com>
2025-06-14 17:49:13 +0800

    fix(platform): correct object_destroy call in can_destroy function
    - Update the can_destroy function to properly call object_destroy
    - Prevent potential issues with incorrect memory handling

--- source/required/platform/communication/can/can.c
+++ source/required/platform/communication/can/can.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-06-09
+ * @date 2025-06-14
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -251,7 +251,8 @@ i32 can_destroy(struct CAN **self)
         ASSERT(self);
 
         if (*self) {
-                object_destroy((struct OBJECT *)*self);
+                struct OBJECT *p = (*self)->super;
+                object_destroy(&p);
                 memory_clear(*self, sizeof(struct CAN));
                 *self = 0;
         }
