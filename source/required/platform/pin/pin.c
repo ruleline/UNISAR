596bb1c0fe7bb89a23bf2be3acfcfb3a6aafbc06
ruleline <ruleline@outlook.com>
2025-06-14 17:52:06 +0800

    fix(pin): update pin_destroy to properly handle object_destroy call
    - Modify pin_destroy function to correctly call object_destroy
    - Prevent potential issues with improper memory handling

--- source/required/platform/pin/pin.c
+++ source/required/platform/pin/pin.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-06-09
+ * @date 2025-06-14
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -171,7 +171,8 @@ i32 pin_destroy(struct PIN **self)
         ASSERT(self);
 
         if (*self) {
-                object_destroy((struct OBJECT *)*self);
+                struct OBJECT *p = (*self)->super;
+                object_destroy(&p);
                 memory_clear(*self, sizeof(struct PIN));
                 *self = 0;
         }
