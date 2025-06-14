55c3a3d59419678002baaecab590a306a8c8ad85
ruleline <ruleline@outlook.com>
2025-06-14 17:49:34 +0800

    fix(spi): correct object_destroy usage in spi_destroy function
    - Update spi_destroy function to properly handle object_destroy call
    - Prevent potential issues with incorrect memory access and destruction

--- source/required/platform/communication/spi/spi.c
+++ source/required/platform/communication/spi/spi.c
@@ -5,7 +5,7 @@
  * @since 2025-02-18
  *
  * @authors ruleline (ruleline@outlook.com)
- * @date 2025-06-09
+ * @date 2025-06-14
  * @version 0.00.001
  *
  * @copyright ©2025 UNISAR
@@ -218,7 +218,8 @@ i32 spi_destroy(struct SPI **self)
         ASSERT(self);
 
         if (*self) {
-                object_destroy((struct OBJECT *)*self);
+                struct OBJECT *p = (*self)->super;
+                object_destroy(&p);
                 memory_clear(*self, sizeof(*self));
                 *self = 0;
         }
